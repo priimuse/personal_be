f1623d3c3d57156e7f644d46cd4b5427ae7bc0ba
priimuse <wmwilson87@gmail.com>
2024-04-10 07:48:20 -0700

    port requirements per the heroku docs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //heroku app at 
 //https://immense-sands-12203-f6a7846ce7c0.herokuapp.com/
